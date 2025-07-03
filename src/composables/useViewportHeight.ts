072ebf8b107f0e5911044a143806ea64ffd738c5
Tina <tina@tripguru360.com>
2025-07-03 20:47:48 +0800

    fix: webview viewport height

--- src/composables/useViewportHeight.ts
+++ src/composables/useViewportHeight.ts
@@ -2,7 +2,10 @@ import { onMounted, onBeforeUnmount } from 'vue'
 
 export function useViewportHeight() {
   const setVH = () => {
-    const vh = window.innerHeight * 0.01
+    const vh =
+      window.visualViewport?.height != null
+        ? window.visualViewport.height * 0.01
+        : window.innerHeight * 0.01
     document.documentElement.style.setProperty('--vh', `${vh}px`)
   }
 
